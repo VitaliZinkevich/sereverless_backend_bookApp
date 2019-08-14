2b449f6b4ac484a3b433b5ae9622970fc609256b
vitalizinkevich <zinkevich@nca.by>
2019-08-14 11:54:35 +0300

    add orderList with dep on role

--- orders/getOrder.js
+++ orders/getOrder.js
@@ -1,99 +1,72 @@
-// const mongoose = require('mongoose');
-// const Promise = require('bluebird');
-// const validator = require('validator');
-// const UserModel = require('./model/User.js');
-// const HotelModel = require ('./model/')
-
+const AWS = require('aws-sdk')
+const cognito = new AWS.CognitoIdentityServiceProvider();
 const OrderModel = require ('./model/Order')
 
 let util = require('./utils');
 
 module.exports.getOrder = (event, context, callback) => {
-  // if (!validator.isAlphanumeric(event.pathParameters.id)) {
-  //   callback(null, util.createErrorResponse(400, 'Incorrect id'));
-  //   return;
-  // }
 
-  util.dbConnectAndExecute(util.mongoString, () => (
-    OrderModel
-      .find({})
-      .then(hotels => callback(null, { statusCode: 200, body: JSON.stringify(hotels),
-        headers: {  
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true }, }))
-      .catch(err => callback(null, util.createErrorResponse(err.statusCode, err.message)))
-  ));
+    //USER IDENTITY START < ================
+
+
+    const authProvider = event.requestContext.identity.cognitoAuthenticationProvider;
+    // cognito authentication provider looks like:
+    // cognito-idp.us-east-1.amazonaws.com/us-east-1_xxxxxxxxx,cognito-idp.us-east-1.amazonaws.com/us-east-1_xxxxxxxxx:CognitoSignIn:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+    const parts = authProvider.split(':');
+    const userPoolIdParts = parts[parts.length - 3].split('/');
+    const userPoolId = userPoolIdParts[userPoolIdParts.length - 1];
+    const userPoolUserId = parts[parts.length - 1];
+  
+    // async function  getAttr (){
+    //   try {
+    //     const user = await cognito.adminGetUser({
+    //       UserPoolId:userPoolId,
+    //       Username: userPoolUserId
+    //     }).promise()
+    //     .then (res=>{
+    //       console.log('then getGroup')
+    //       console.log(res)
+    //     })
+    //     //console.log(user.UserAttributes)
+    //   } catch (err) {
+    //     console.log(err)
+    //   }
+    // }
+  
+    async function  getGroup (){
+      try {
+        const group = await cognito.adminListGroupsForUser({
+          UserPoolId:userPoolId,
+          Username: userPoolUserId
+        }).promise()
+        // console.log(JSON.stringify (group))
+        // console.lo(JSON.stringify (group.Groups[0].GroupName));
+        let findOpt = {};
+        if (!!group.Groups.length && group.Groups[0].GroupName === 'admin'){
+          // return all
+        } else {
+          findOpt.userId = userPoolUserId;
+        }
+        
+        util.dbConnectAndExecute(util.mongoString, () => (
+          OrderModel
+            .find({...findOpt})
+            .then(orders => callback(null, { statusCode: 200, body: JSON.stringify(orders),
+              headers: {  
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true }, }))
+            .catch(err => callback(null, util.createErrorResponse(err.statusCode, err.message)))
+        ));        
+        
+
+
+      } catch (err) {
+        console.log(err)
+      }
+    }
+   
+    // getAttr();
+    getGroup();
+    //USER IDENTITY END < ================
+
 };
-
-
-// module.exports.createHotel = (event, context, callback) => {
-//   const data = JSON.parse(event.body);
-
-//   const user = new HotelModel({
-//     type: data.type,
-//     name: data.name,
-//     region: data.region,
-//     stars: data.stars,
-//     rooms: data.rooms,
-//     description:data.description
-//   });
-
-  // if (user.validateSync()) {
-  //   callback(null, createErrorResponse(400, 'Incorrect user data'));
-  //   return;
-  // }
-
-//   dbConnectAndExecute(mongoString, () => (
-//     user
-//       .save()
-//       .then(() => callback(null, {
-//         statusCode: 200,
-//         body: JSON.stringify({ id: user.id }),
-//       }))      
-//       .catch(err => callback(null, createErrorResponse(err.statusCode, err.message)))
-//   ));
-// };
-
-// module.exports.deleteUser = (event, context, callback) => {
-//   if (!validator.isAlphanumeric(event.pathParameters.id)) {
-//     callback(null, createErrorResponse(400, 'Incorrect id'));
-//     return;
-//   }
-
-//   dbConnectAndExecute(mongoString, () => (
-//     UserModel
-//       .remove({ _id: event.pathParameters.id })
-//       .then(() => callback(null, { statusCode: 200, body: JSON.stringify('Ok') }))
-//       .catch(err => callback(null, createErrorResponse(err.statusCode, err.message)))
-//   ));
-// };
-
-// module.exports.updateUser = (event, context, callback) => {
-//   const data = JSON.parse(event.body);
-//   const id = event.pathParameters.id;
-
-//   if (!validator.isAlphanumeric(id)) {
-//     callback(null, createErrorResponse(400, 'Incorrect id'));
-//     return;
-//   }
-
-//   const user = new UserModel({
-//     _id: id,
-//     name: data.name,
-//     firstname: data.firstname,
-//     birth: data.birth,
-//     city: data.city,
-//     ip: event.requestContext.identity.sourceIp,
-//   });
-
-//   if (user.validateSync()) {
-//     callback(null, createErrorResponse(400, 'Incorrect parameter'));
-//     return;
-//   }
-
-//   dbConnectAndExecute(mongoString, () => (
-//     UserModel.findByIdAndUpdate(id, user)
-//       .then(() => callback(null, { statusCode: 200, body: JSON.stringify('Ok') }))
-//       .catch(err => callback(err, createErrorResponse(err.statusCode, err.message)))
-//   ));
-// };

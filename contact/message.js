814b3204b2c141b33bc13b78ea9ab93de40535fd
vitalizinkevich <zinkevich@nca.by>
2019-08-20 16:47:00 +0300

    contact service with aws ses

--- contact/message.js
+++ contact/message.js
@@ -1,69 +1,10 @@
 'use strict';
 const AWS = require("aws-sdk");
-var nodemailer = require('nodemailer');
 
 module.exports.messageRecieved = function(event, context, callback) {
 
   let {email , message} = JSON.parse (event.body);
   console.log(email , message);
-
-  // ------------------------------- START NODEMAILER -----------------------
-  var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-           user: 'vitalizinkevich@gmail.com',
-           pass: 'kie#xaiB$u1quei'
-       }
-   });
-
-   const mailOptionsToContacter = {
-    // from: 'sender@email.com', // sender address
-    to: email, // list of receivers
-    subject: 'Ваше сообщение получено', // Subject line
-    html: '<p>Ваше сообщение получено, ответ на него придет на указанною вами почту.</p>'// plain text body
-  };
-
-  const mailOptionsToAdmin = {
-    // from: 'sender@email.com', // sender address
-    to: 'vitalizinkevich@gmail.com', // list of receivers
-    subject: 'Получено обращение с сайта', // Subject line
-  html: `<p>Почта ${email} <br/>
-            Сообщение ${message}</p>`// plain text body
-  };
-
-
-  let contacter = transporter.sendMail(mailOptionsToContacter, function (err, info) {
-    if(err)
-      console.log(err)
-    else
-      console.log(info);
- });
-
- let admin = transporter.sendMail(mailOptionsToAdmin, function (err, info) {
-  if(err)
-    console.log(err)
-  else
-    console.log(info);
-});
-
-Promise.all ([admin, contacter]).then (()=> callback(null, {
-  statusCode: 200,
-  body: JSON.stringify({message: 'Email was sent'}),
-  headers: {  
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true },
-}))
-
- callback(null, {
-  statusCode: 200,
-  body: JSON.stringify({message: 'Email was sent'}),
-  headers: {  
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true },
-})
-
-  // ------------------------------- END NODEMAILER -----------------------
-
   // ------------------------------- START SES ----------------------------
   //AWS.config.update({ region: "eu-west-1" });
   // console.log('Handling confirmation email to', event);
@@ -76,61 +17,82 @@ Promise.all ([admin, contacter]).then (()=> callback(null, {
 
   // // const name = event.name.substr(0, 40).replace(/[^\w\s]/g, '');
 
-  // const htmlBody = `
-  //   <!DOCTYPE html>
-  //   <html>
-  //     <head>
-  //     </head>
-  //     <body>
-  //       <p>Hi Vitali,</p>
-  //       <p>...</p>
-  //     </body>
-  //   </html>
-  // `;
+  const htmlBody = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+      </head>
+      <body>
+        <p>Ваше сообщение получено, мы ответим как можно скорее.</p>
+      </body>
+    </html>
+  `;
 
-  // const textBody = `
-  //   Hi name,
-  //   ...
-  // `;
+  const textBody = `
+    текст тела письма
+  `;
 
-  // // Create sendEmail params
-  // const params = {
-  //   Destination: {
-  //     ToAddresses: ['vitalizinkevich@gmail.com']
-  //   },
-  //   Message: {
-  //     Body: {
-  //       Html: {
-  //         Charset: "UTF-8",
-  //         Data: htmlBody
-  //       },
-  //       Text: {
-  //         Charset: "UTF-8",
-  //         Data: textBody
-  //       }
-  //     },
-  //     Subject: {
-  //       Charset: "UTF-8",
-  //       Data: "Thanks for registering with ACME!"
-  //     }
-  //   },
-  //   Source: "vitalizinkevich@gmail.com"
-  // };
+  // Create sendEmail params
+  const paramsContacter = {
+    Destination: {
+      ToAddresses: [`${email}`]
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: htmlBody
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: textBody
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Сообщение сайта"
+      }
+    },
+    Source: "vitalizinkevich@gmail.com"
+  };
+  const paramsAdmin = {
+    Destination: {
+      ToAddresses: [`vitalizinkevich@gmail.com`]
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: `Сообщение ${message} от  ${email}`
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: textBody
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Сообщение сайта"
+      }
+    },
+    Source: "vitalizinkevich@gmail.com"
+  };
 
-  // // Create the promise and SES service object
-  // const sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
-  //   .sendEmail(params)
-  //   .promise();
+  // Create the promise and SES service object
+  const sendContacter = new AWS.SES({ apiVersion: "2010-12-01" })
+    .sendEmail(paramsContacter)
+    .promise();
 
-  // // Handle promise's fulfilled/rejected states
-  // sendPromise
-  //   .then(data => {
-  //     console.log(data.MessageId);
-  //     context.done(null, "Success");
-  //   })
-  //   .catch(err => {
-  //     console.error(err, err.stack);
-  //     context.done(null, "Failed");
-  //   });
-    // ------------------------------- END SES ----------------------------
+    const sendAdmin = new AWS.SES({ apiVersion: "2010-12-01" })
+    .sendEmail(paramsAdmin)
+    .promise();
+
+  Promise.all ([sendContacter, sendAdmin]).then (()=> callback(null, {
+  statusCode: 200,
+  body: JSON.stringify({message: 'Email was sent'}),
+  headers: {  
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true },
+  })).catch (err => console.log(err))
+  // ------------------------------- END SES ----------------------------
 };
